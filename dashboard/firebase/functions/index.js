f1a25d051e6c93081afa35316f24cdc54d312a44
iamtrask <liamtrask@gmail.com>
2020-03-04 20:46:42 +0000

    Changes

--- dashboard/firebase/functions/index.js
+++ dashboard/firebase/functions/index.js
@@ -1,5 +1,10 @@
 const functions = require('firebase-functions');
+   
+// import paillier
+const paillier = require('paillier-bigint');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('ANDREW - FILL THIS IN', request);
+  console.log(paillier)
+  response.send(JSON.stringify({"request":request.query.text}))
+  // response.send('ANDREW - FILL THIS IN', request);
 });
