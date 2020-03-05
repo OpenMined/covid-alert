684ffe85d0476cb356abe3ea652d65122554ebef
Patrick Cason <me@patrickcason.com>
2020-03-05 17:53:57 +0000

    Finished, more or less

--- dashboard/src/components/LocationsList.js
+++ dashboard/src/components/LocationsList.js
@@ -6,19 +6,24 @@ export default ({ locations, ...props }) => (
     <Heading as="h3" size="lg">
       Locations
     </Heading>
-    <List spacing={2} mt={2}>
-      {locations.map(({ lng, lat }) => {
-        return (
-          <ListItem key={`location-${lng},${lat}`}>
-            <Text>
-              <strong>Longitude:</strong> {lng}
-            </Text>
-            <Text>
-              <strong>Latitude:</strong> {lat}
-            </Text>
-          </ListItem>
-        );
-      })}
-    </List>
+    {locations.length >= 1 && (
+      <List spacing={2} mt={2}>
+        {locations.map(({ lng, lat }) => {
+          return (
+            <ListItem key={`location-${lng},${lat}`}>
+              <Text>
+                <strong>Longitude:</strong> {lng}
+              </Text>
+              <Text>
+                <strong>Latitude:</strong> {lat}
+              </Text>
+            </ListItem>
+          );
+        })}
+      </List>
+    )}
+    {locations.length === 0 && (
+      <Text mt={2}>We don't have any locations for this patient yet.</Text>
+    )}
   </Box>
 );
