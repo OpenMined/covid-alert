579f400c300631ec2d5fcb172e32d17304d50971
Patrick Cason <me@patrickcason.com>
2020-03-06 20:52:10 +0000

    Server function is close, but logic is wrong

--- dashboard/firebase/functions/index.js
+++ dashboard/firebase/functions/index.js
@@ -12,6 +12,22 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+const loopGridTensor = (grid, func) => {
+  const newGrid = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    const newGridRow = [];
+
+    for (let j = 0; j < grid[i].length; j++) {
+      newGridRow.push(func(grid[i][j], i, j));
+    }
+
+    newGrid.push(newGridRow);
+  }
+
+  return newGrid;
+};
+
 exports.performSectorMatch = functions.https.onRequest((req, res) => {
   const { sectorKey } = req.body;
 
@@ -37,31 +53,13 @@ exports.performGridTensorComputation = functions.https.onRequest(
     // const { sectorKey, gridTensor, publicKey } = req.body;
 
     const sectorKey = 'hello';
-    const { publicKey, privateKey } = await paillier.generateRandomKeys(16);
+    const { publicKey, privateKey } = await paillier.generateRandomKeys(1024);
 
     const weakGridTensor = gps2box(33.77380000000002, -84.2961);
-
-    const loopGridTensor = (gridTensor, func) => {
-      const newGridTensor = [];
-
-      for (let i = 0; i < gridTensor.length; i++) {
-        const newGridTensorRow = [];
-
-        for (let j = 0; j < gridTensor[i].length; j++) {
-          newGridTensorRow.push(func(gridTensor[i][j], i, j));
-        }
-
-        newGridTensor.push(newGridTensorRow);
-      }
-
-      return newGridTensor;
-    };
 
     const gridTensor = loopGridTensor(weakGridTensor.gridTensor, val =>
       publicKey.encrypt(val)
     );
-
-    console.log(gridTensor);
 
     db.collectionGroup('locations')
       .where('sector_key', '==', sectorKey)
@@ -80,29 +78,27 @@ exports.performGridTensorComputation = functions.https.onRequest(
             const convertedLocation = gps2box(location.lat, location.lng);
             const patientGridTensor = convertedLocation.gridTensor;
 
-            console.log(patientGridTensor.convertedLocation.gridTensor);
-
             // Multiply that against the grid tensor from the user
-            const eMulVal = loopGridTensor(gridTensor, (v, i, j) =>
+            let eMulVal = loopGridTensor(gridTensor, (v, i, j) =>
               publicKey.multiply(gridTensor[i][j], patientGridTensor[i][j])
             );
 
-            console.log(eMulVal);
-            // const eMulVal = publicKey.multiply(gridTensor, patientGridTensor);
-
-            // if (eOverlapGridTensor) {
-            //   // If eOverlapGridTensor already has a value, add it to the new eMulVal
-            //   eOverlapGridTensor = publicKey.addition(
-            //     eOverlapGridTensor,
-            //     eMulVal
-            //   );
-            // } else {
-            //   // If eOverlapGridTensor isn't set yet, set it to eMulVal
-            eOverlapGridTensor = eMulVal;
-            // }
+            if (eOverlapGridTensor) {
+              // If eOverlapGridTensor already has a value, add it to the new eMulVal
+              eOverlapGridTensor = loopGridTensor(gridTensor, (v, i, j) =>
+                publicKey.addition(eOverlapGridTensor[i][j], eMulVal[i][j])
+              );
+            } else {
+              // If eOverlapGridTensor isn't set yet, set it to eMulVal
+              eOverlapGridTensor = eMulVal;
+            }
           });
 
-          console.log('FINAL', privateKey.decrypt(eOverlapGridTensor));
+          let finalDecryptedArray = loopGridTensor(eOverlapGridTensor, val =>
+            privateKey.decrypt(val)
+          );
+
+          console.log('FINAL', finalDecryptedArray);
 
           // Send the resulting tensor
           return res.send(JSON.stringify({ result: eOverlapGridTensor }));
@@ -117,40 +113,3 @@ exports.performGridTensorComputation = functions.https.onRequest(
       });
   }
 );
-
-const runClient = async (lat, lng) => {
-  // Create a random public/private key pair for the Paillier cryptosystem
-  const { publicKey, privateKey } = await paillier.generateRandomKeys(3072);
-
-  // Convert latitude and longitude into a sectorKey and gridTensor
-  const { sectorKey, gridTensor } = gps2box(lat, lng);
-
-  // Send just the sectorKey
-  console.log('SENDING THE SECTOR KEY', sectorKey);
-
-  // TODO: CHANGE ME HERE!!!
-  // We have a positive sector match!
-  const HAVE_POSITIVE_SECTOR_MATCH_CHANGE_ME = true;
-
-  if (HAVE_POSITIVE_SECTOR_MATCH_CHANGE_ME) {
-    // Encrypt the gridTensor
-    const encryptedGridTensor = publicKey.encrypt(gridTensor);
-
-    console.log(
-      'SENDING THE SECTOR KEY, ENCRYPTED GRID TENSOR, AND PUBLIC KEY',
-      sectorKey,
-      encryptedGridTensor,
-      publicKey
-    );
-
-    const FAKE_GRID_TENSOR_RESPONSE_CHANGE_ME = [];
-
-    const decryptedServerGridTensor = privateKey.decrypt(
-      FAKE_GRID_TENSOR_RESPONSE_CHANGE_ME
-    );
-
-    if (decryptedServerGridTensor.some(v => v >= 1)) {
-      console.log('THROW A PUSH NOTIFICATION TO GET OUT OF THE AREA');
-    }
-  }
-};
