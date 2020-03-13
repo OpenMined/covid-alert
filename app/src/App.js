3d732b19b35e28b0cde2290a676dd69ba6ea3547
Patrick Cason <me@patrickcason.com>
2020-03-13 16:53:29 +0000

    Log

--- app/src/App.js
+++ app/src/App.js
@@ -217,7 +217,7 @@ export default class extends Component {
     PushNotification.configure({
       // Required: called when a remote or local notification is opened or received
       onNotification: notification => {
-        console.log("NOTIFICATION:", notification);
+        console.log("Push notification", notification);
 
         // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/react-native-push-notification-ios)
         notification.finish(PushNotificationIOS.FetchResult.NoData);
