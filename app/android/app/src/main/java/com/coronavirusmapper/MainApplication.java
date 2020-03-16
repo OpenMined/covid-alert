c5cf8316ac05f7b3fa62b717d3c76d239a6cef62
Thiago Costa Porto <thiagocostaporto@gmail.com>
2020-03-16 01:31:30 +0000

    Android setting update

--- app/android/app/src/main/java/com/coronavirusmapper/MainApplication.java
+++ app/android/app/src/main/java/com/coronavirusmapper/MainApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
-import com.bitgo.randombytes.RandomBytesPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
