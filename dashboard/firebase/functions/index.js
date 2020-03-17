c16e32565fcfaeecad1b713b8f8ffb08ca31ebab
Patrick Cason <me@patrickcason.com>
2020-03-17 16:29:06 +0000

    Redoing the firebase code for betterness

--- dashboard/firebase/functions/index.js
+++ dashboard/firebase/functions/index.js
@@ -1,23 +1,32 @@
-const express = require("express");
-const cors = require("cors");
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const paillier = require("paillier-bigint");
-const { gps2box, stringifyBigInt, parseBigInt } = require("gps-sector-grid");
+const express = require('express');
+const cors = require('cors');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const paillier = require('paillier-bigint');
+// const paillier = require('paillier-pure');
+const { gps2box, stringifyBigInt, parseBigInt } = require('gps-sector-grid');
 
-const serviceAccount = require("./coronavirus-mapper-firebase-adminsdk-i6ree-699f4198bb.json");
+const serviceAccount = require('./coronavirus-mapper-firebase-adminsdk-i6ree-699f4198bb.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://coronavirus-mapper.firebaseio.com"
+  databaseURL: 'https://coronavirus-mapper.firebaseio.com'
 });
 
+// Connect to our database
 const db = admin.firestore();
 
+// The main computation endpoint
 const gridTensorComputation = async (req, res) => {
+  // Get the sectorKey, gridTensor, and publicKey values from the request
   let { sectorKey, gridTensor, publicKey } = parseBigInt(req.body);
 
+  // Generate a public key identical to that given to us by the user
   publicKey = new paillier.PublicKey(publicKey.n, publicKey.g);
+
+  // This is a number which will hold the result of adding all the products computed below together
+  // If there are not matching and patient verified location, we simply return this value unaltered
+  let finalEVal = publicKey.encrypt(0);
 
   // Only work with locations of patients in the last 72 hours
   const recency = Date.now() - 259200000; // 259200000ms === 72 hours
@@ -25,16 +34,13 @@ const gridTensorComputation = async (req, res) => {
 
   // Get all matching locations
   const matchingLocations = await db
-    .collectionGroup("locations")
-    .where("sector_key", "==", sectorKey)
-    .where("last_time", ">=", recencyDate)
+    .collectionGroup('locations')
+    .where('sector_key', '==', sectorKey)
+    .where('last_time', '>=', recencyDate)
     .get();
 
   // If we have one or more matching locations...
   if (matchingLocations.size >= 1) {
-    // An array for holding the resulting grid tensors of all multiplications on patientLocations, summed together
-    let eOverlapGridTensor;
-
     // For each confirmed location in this sector in the last 72 hours
     for (let doc of matchingLocations.docs) {
       // eslint-disable-next-line no-await-in-loop
@@ -52,37 +58,24 @@ const gridTensorComputation = async (req, res) => {
         const convertedLocation = gps2box(location.lat, location.lng);
         const patientGridTensor = [].concat(...convertedLocation.gridTensor);
 
-        // Multiply that against the grid tensor from the user
+        // Create a value to store the product of gridTensor values and patientGridTensor values
         let eMulVal = [];
 
+        // Multiply the gridTensor values and patientGridTensor values
         for (let i = 0; i < gridTensor.length; i++) {
           eMulVal.push(publicKey.multiply(gridTensor[i], patientGridTensor[i]));
         }
 
-        if (eOverlapGridTensor) {
-          // If eOverlapGridTensor already has a value, add it to the new eMulVal
-          const tempEOverlapGridTensor = [];
-
-          for (let i = 0; i < gridTensor.length; i++) {
-            tempEOverlapGridTensor.push(
-              publicKey.addition(eOverlapGridTensor[i], eMulVal[i])
-            );
-          }
-
-          eOverlapGridTensor = tempEOverlapGridTensor;
-        } else {
-          // If eOverlapGridTensor isn't set yet, set it to eMulVal
-          eOverlapGridTensor = eMulVal;
+        // Add all those numbers togther and store them in finalEVal
+        for (let i = 0; i < eMulVal.length; i++) {
+          finalEVal = publicKey.addition(finalEVal[i], eMulVal[i]);
         }
       }
     }
-
-    // Send the resulting tensor
-    return res.send({ result: stringifyBigInt(eOverlapGridTensor) });
   }
 
-  // We have no matches
-  return res.send(JSON.stringify({ matches: false }));
+  // Send the result
+  return res.send(stringifyBigInt({ result: finalEVal }));
 };
 
 // Declare an Express.js app
@@ -92,7 +85,7 @@ const app = express();
 app.use(cors({ origin: true }));
 
 // Build our routes
-app.post("/grid-tensor-computation", gridTensorComputation);
+app.post('/grid-tensor-computation', gridTensorComputation);
 
 // Expose Express API as a single Cloud Function:
 exports.api = functions.https.onRequest(app);
@@ -102,7 +95,7 @@ exports.createPatient = functions.auth.user().onCreate(user => {
   const isAdminUser = /^[\w.+-]+@(un\.org|who\.int)$/.test(user.email);
 
   if (!isAdminUser) {
-    db.collection("patients")
+    db.collection('patients')
       .doc(user.uid)
       .set({
         created_at: admin.firestore.FieldValue.serverTimestamp(),
