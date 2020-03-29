9c129600c9929a7c73d2917f64ce1f9baa1c9ab9
Kevin Wang <kevmo314@gmail.com>
2020-03-29 13:32:04 -0400

    Revert AddLocationForm.

--- dashboard/src/components/AddLocationForm.js
+++ dashboard/src/components/AddLocationForm.js
@@ -15,67 +15,65 @@ export default ({ lat, lng, doLocationAdd }) => {
   const { handleSubmit, errors, register, formState } = useForm();
 
   return (
-    <React.Fragment>
-      <form
-        onSubmit={handleSubmit(values => {
-          values.lat = lat;
-          values.lng = lng;
+    <form
+      onSubmit={handleSubmit(values => {
+        values.lat = lat;
+        values.lng = lng;
 
-          doLocationAdd(values);
-        })}
-      >
-        <Stack spacing={4}>
-          <Box>
-            <Text fontWeight="bold">Latitude</Text>
-            <Text>{lat.toFixed(5)}</Text>
-          </Box>
-          <Box>
-            <Text fontWeight="bold">Longitude</Text>
-            <Text>{lng.toFixed(5)}</Text>
-          </Box>
-          <FormControl isInvalid={errors.date}>
-            <FormLabel htmlFor="date">Last estimated date</FormLabel>
-            <Input
-              name="date"
-              placeholder="DD/MM/YYYY"
-              ref={register({
-                required: "Date is required",
-                pattern: {
-                  value: /^(\d{2})\/(\d{2})\/(\d{4})$/,
-                  message: "Invalid date"
-                }
-              })}
-            />
-            <FormErrorMessage>
-              {errors.date && errors.date.message}
-            </FormErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={errors.time}>
-            <FormLabel htmlFor="time">Last estimated time</FormLabel>
-            <Input
-              name="time"
-              placeholder="HH:MM"
-              ref={register({
-                required: "Time is required",
-                pattern: {
-                  value: /^(\d{2}):(\d{2})$/,
-                  message: "Invalid time"
-                }
-              })}
-            />
-            <FormErrorMessage>
-              {errors.time && errors.time.message}
-            </FormErrorMessage>
-          </FormControl>
-          <Button
-            variantColor="blue"
-            isLoading={formState.isSubmitting}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Stack>
-      </form>
-    </React.Fragment>
+        doLocationAdd(values);
+      })}
+    >
+      <Stack spacing={4}>
+        <Box>
+          <Text fontWeight="bold">Latitude</Text>
+          <Text>{lat.toFixed(5)}</Text>
+        </Box>
+        <Box>
+          <Text fontWeight="bold">Longitude</Text>
+          <Text>{lng.toFixed(5)}</Text>
+        </Box>
+        <FormControl isInvalid={errors.date}>
+          <FormLabel htmlFor="date">Last estimated date</FormLabel>
+          <Input
+            name="date"
+            placeholder="DD/MM/YYYY"
+            ref={register({
+              required: "Date is required",
+              pattern: {
+                value: /^(\d{2})\/(\d{2})\/(\d{4})$/,
+                message: "Invalid date"
+              }
+            })}
+          />
+          <FormErrorMessage>
+            {errors.date && errors.date.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl isInvalid={errors.time}>
+          <FormLabel htmlFor="time">Last estimated time</FormLabel>
+          <Input
+            name="time"
+            placeholder="HH:MM"
+            ref={register({
+              required: "Time is required",
+              pattern: {
+                value: /^(\d{2}):(\d{2})$/,
+                message: "Invalid time"
+              }
+            })}
+          />
+          <FormErrorMessage>
+            {errors.time && errors.time.message}
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          variantColor="blue"
+          isLoading={formState.isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Stack>
+    </form>
   );
 };
