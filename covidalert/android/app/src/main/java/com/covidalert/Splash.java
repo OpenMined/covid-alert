e4dc3dffedd1ce60b8af9f3a1344bf867e213d33
Inam Khan <inamkh0001@gmail.com>
2020-03-29 15:12:46 +0530

    added android splash screen

---
+++ covidalert/android/app/src/main/java/com/covidalert/Splash.java
@@ -0,0 +1,14 @@
+package com.covidalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
