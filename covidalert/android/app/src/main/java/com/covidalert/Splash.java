7b3e90adc7ffa0933250132d6987dfb10493cd72
Inam Khan <inamkh0001@gmail.com>
2020-03-29 15:14:44 +0530

    added android splash

--- covidalert/android/app/src/main/java/com/covidalert/Splash.java
+++ covidalert/android/app/src/main/java/com/covidalert/Splash.java
@@ -2,7 +2,9 @@ package com.covidalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        },3000);
     }
 }
