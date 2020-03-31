b4e7b3495a5598b51680853f6d2d31eda6d03e37
Nick Angelou <nick@morfix.io>
2020-03-30 21:08:12 -0500

    chore: simplified the RTL utility helper

--- covidalert/src/view/components/utils/style/index.js
+++ covidalert/src/view/components/utils/style/index.js
@@ -1,23 +1,20 @@
 import i18n from '../../Internationalize/I18n';
 
+/**
+ * Re-Styles a style based on RTL
+ *
+ * @param {Object} s Input style object
+ * @param {Boolean} [rightAlign=false] If it should right align the text
+ * @returns {*}
+ */
 const d = (s, rightAlign = false) => {
   const rtl = i18n.getDir() === 'rtl';
   return rtl
-    ? [
-        s,
-        {
-          direction: 'rtl',
-        },
-        rightAlign
-          ? {
-              alignSelf: 'flex-end',
-              textAlign: 'left',
-            }
-          : {
-              alignSelf: 'flex-start',
-              textAlign: 'right',
-            },
-      ]
+    ? {
+        ...s,
+        direction: 'rtl',
+        textAlign: rightAlign ? 'left' : 'right',
+      }
     : s;
 };
 
